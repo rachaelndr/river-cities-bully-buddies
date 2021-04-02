5406e9d7f454c947505a3efad2687780d71884f3
Rachael <rachaeln@draccelerator.com>
2021-04-02 14:07:08 -0400

    support toggle edits

--- js/about.js
+++ js/about.js
@@ -2,7 +2,9 @@ const toggleList = document.getElementById(`toggleList`);
 const list = document.querySelector(`.list`);
 
 toggleList.addEventListener(`click`, () => {
-    if (list.style.display == `none`) {
+    if (list.style.display == ``) {
+        list.style.display = `block`;
+    } else if (list.style.display == `none`){
         list.style.display = `block`;
     } else {
         list.style.display = `none`;
